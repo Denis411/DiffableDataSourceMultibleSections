b277d0d19edc4427decbf2164ba3a1cf5fd7fe02
Dennis <dnsljnv411@gmail.com>
2021-12-16 10:51:43 +0300

    Add table view

--- DiffableDataSource/ViewController.swift
+++ DiffableDataSource/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var tableView: UITableView!
+    
+    override func loadView() {
+        tableView = UITableView()
+        view = tableView!
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        prettifyTableView()
         
     }
-
-
+    
+    func prettifyTableView() {
+        tableView.backgroundColor = .blue
+    }
 }
 
